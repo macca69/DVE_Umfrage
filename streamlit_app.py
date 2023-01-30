ae983d3f7ed0ea50c44458809d5b99e69ccba3f2
macca69 <47122251+macca69@users.noreply.github.com>
2023-01-30 11:43:16 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ unique_columns = df_1.columns.to_list()
 unique_columns = [k for k in unique_columns if 'Mehrfach' not in k]
 
 erste_achse = st.selectbox('Erste Achse', unique_columns, 13)
-zweite_achse = st.selectbox('Zweite Achse', unique_columns.remove(erste_achse, 12)
+zweite_achse = st.selectbox('Zweite Achse', unique_columns.remove(erste_achse, 12))
 
 typ = st.checkbox("Percent", key="disabled")
 
