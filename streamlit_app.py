4cd6ccf74533625cb69af95be61d9d03cf0e061b
macca69 <47122251+macca69@users.noreply.github.com>
2023-01-30 14:11:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,11 @@ df_1 = pd.read_csv('df_1.csv', index_col=0)
 unique_columns = df_1.columns.to_list()
 unique_columns = [k for k in unique_columns if 'Mehrfach' not in k]
 
-erste_achse = st.selectbox('Erste Achse', unique_columns)
+erste_achse = st.selectbox('Erste Achse', unique_columns, 1)
 
 #unique_columns.remove(erste_achse)
 
-zweite_achse = st.selectbox('Zweite Achse', unique_columns, 1)
+zweite_achse = st.selectbox('Zweite Achse', unique_columns, 3)
 
 col3, col4 = st.columns(2)
 typ = col3.checkbox("Percent")
