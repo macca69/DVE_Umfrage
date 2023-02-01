61daf958a19066c0bacc5d27f4acbe7554ddebe4
macca69 <47122251+macca69@users.noreply.github.com>
2023-02-01 10:34:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ else:
 fig.update_layout(legend=dict(orientation="v", yanchor="top", y=-0.25, xanchor="left", x=0),
                   margin=dict(l=0, r=0, t=40, b=0))
 
-fig.update_xaxes(title=filter1.split(') ')[1], categoryarray=natsorted(histogram[filter1].unique()), categoryorder='array')
+fig.update_xaxes(title=filter1.split(') ')[1], categoryarray=natsorted(df_1[filter1].unique()), categoryorder='array')
 fig.update_yaxes(title='Anzahl '+filter2.split(') ')[1])#, categoryarray=natsorted(histogram[filter2].unique()), categoryorder='array')
 
 st.plotly_chart(fig, use_container_width=True)
