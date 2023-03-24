1a9693d25b5e2f1121ce5e434675266078a51eea
macca69 <47122251+macca69@users.noreply.github.com>
2023-03-24 19:28:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ st.dataframe(df_slice_3, use_container_width=True)
 
 st.write(df_slice_2[[filter1, filter2]].value_counts().to_frame().rename(columns={0: 'counts'}).reset_index())
 
-fig = px.histogram(df_slice_2[[filter1, filter2]].value_counts().to_frame().rename(columns={0: 'counts'}),
+fig = px.histogram(df_slice_2[[filter1, filter2]].value_counts().to_frame().rename(columns={0: 'counts'}).reset_index(),
                    x=filter1, y='counts', color=filter2, barnorm='', text_auto='.1f', width=1000, height=750)
 
 fig.update_layout(legend=dict(orientation="v", yanchor="top", y=-0.25, xanchor="left", x=0),
