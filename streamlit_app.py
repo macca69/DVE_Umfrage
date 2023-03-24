0ab7b6c85802c2375dc0a3c2afccf0a9b3c191d4
macca69 <47122251+macca69@users.noreply.github.com>
2023-03-24 21:18:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,8 +58,12 @@ with st.expander('Datensatz'):
     #st.dataframe(df_slice_2, use_container_width=True)
     st.dataframe(df_slice_3, use_container_width=True)
 
+if st.checkbox("Percent"):
+        barnorm='percent'
+    else:
+        barnorm=''
 fig = px.histogram(df_slice_2[[filter1, filter2]].value_counts().to_frame().rename(columns={0: 'counts'}).reset_index(),
-                   x=filter1, y='counts', color=filter2, barnorm='', text_auto='.1f', width=1000, height=750)
+                   x=filter1, y='counts', color=filter2, barnorm=barnorm, text_auto='.1f', width=1000, height=750)
 
 fig.update_layout(legend=dict(orientation="v", yanchor="top", y=-0.25, xanchor="left", x=0),
                   margin=dict(l=0, r=0, t=40, b=0))
@@ -68,6 +72,10 @@ fig.update_yaxes(title='Anzahl')
 st.plotly_chart(fig, use_container_width=True)
 
 
+if st.checkbox("Percent"):
+        barnorm='percent'
+    else:
+        barnorm=''
 fig2 = px.histogram(df_slice_3[[filter2, filter3]].value_counts().to_frame().rename(columns={0: 'counts'}).reset_index(),
                    x=filter2, y='counts', color=filter3, barnorm='', text_auto='.1f', width=1000, height=750)
 
