5b063ef6d820a5673255fa4cb56a97656fdf4ea7
macca69 <47122251+macca69@users.noreply.github.com>
2023-03-26 18:55:16 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -90,7 +90,7 @@ st.write(filter1_items)
 for ii in filter1_items:
     temporary_3 = df_1[df_1[filter1]==ii]
     if filter2 in mehrfach:
-        st.write(pd.Series(flatten([k for k in temporary_1[filter2]])).value_counts().to_frame().assign(filter1=ii))
+        st.write(pd.Series(flatten([k for k in temporary_3[filter2]])).value_counts().to_frame().assign(filter1=ii))
         temporary_2.append(pd.Series(flatten([k for k in temporary_3[filter2]])).value_counts().to_frame().assign(filter1=ii))
         
 temporary_2 = pd.concat(temporary_2).reset_index().rename(columns={'index': filter2, 0: 'counts', 'filter1': filter1})
