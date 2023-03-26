483a623f83fd96b9587d74571fac1c48ad391f0c
macca69 <47122251+macca69@users.noreply.github.com>
2023-03-26 18:59:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,9 +95,9 @@ for ii in filter1_items:
         
 temporary_2 = pd.concat(temporary_2).reset_index().rename(columns={'index': filter2, 0: 'counts', 'filter1': filter1})
 st.write(temporary_2)
-plot_and_layout(temporary_2, filter1, filter2, filter1_items, barnorm)
+plot_and_layout(temporary_2, filter1, filter2, filter1_items, False)
 st.stop()
-    
+###########################################################    
 col6, col7 = st.columns(2)
 
 with col6:
