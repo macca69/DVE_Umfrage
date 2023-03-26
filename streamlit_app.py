bf926dc7a98dcbbb6b9a60ca5164cda9c8444258
macca69 <47122251+macca69@users.noreply.github.com>
2023-03-26 23:42:25 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,6 +68,8 @@ filter2 = col4.selectbox('Zweiter Filter', unique_columns, 2)
 with col4.expander('Kategorien wählen'):
     
     if filter2 in mehrfach:
+        st.write(df_1[filter2])
+        st.write(flatten([k for k in df_1[filter2]])
         filter2_items = st.multiselect('Kategorien wählen', natsorted(df_1[filter2].unique()), natsorted(df_1[filter2].unique()), label_visibility='collapsed')
         
     else:
