41629b6acdd11d9455caf5a53ab28296bd62e938
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-01 23:48:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,9 @@ def significance_test(df, filter1, filter2, filter1_items, filter2_items):
 
     # Gib die Testergebnisse aus
     with st.expander('Einzelheiten Signifikanztest'):
-        st.write(filter1, filter2, filter1_items, filter2_items)
+        st.write(filter1, filter2)
+        st.write(filter1_items, [x for x in filter1_items if x == x])
+        st.write(filter2_items, [x for x in filter2_items if x == x])
         st.dataframe(pd.DataFrame(np.array(df.pivot(index=filter1, columns=filter2, values='counts').fillna(0)).astype(int), columns=filter2_items, index=filter1_items),  use_container_width=True)
         st.text("Chi-Quadrat-Statistik = " + str(chi2_stat))
         st.text("p-Wert = " + str(p_val))
