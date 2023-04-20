6ef514d8441ab9fc113a3d38817cb7cd7c8f09ca
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-21 01:03:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,7 @@ def plot_and_layout(fig_data, filter1, filter2, barnorm):
     #fig_data = fig_data.reindex(index=order_by_index(fig_data.index, index_natsorted(fig_data.filter2)))
     fig_data_natsorted = []
     for k in natsorted(fig_data[filter2].unique()):
+        st.write(k)
         fig_data_natsorted.append(fig_data[fig_data.filter2==k])
     fig_data = pd.concat(fig_data_natsorted, axis=0)
     
