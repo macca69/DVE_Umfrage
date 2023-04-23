e58528e02467ae9e5ec004d939c121cd3f0b211f
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-23 14:10:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,7 +127,7 @@ if filter2 == 'keiner':
     font_size_factor = st.number_input('Schriftgröße', min_value=0.5, max_value=2.0, value=0.5, step=0.1, key='57')
     
     fig = px.histogram(df_1[filter1], text_auto='.0f')
-    st.write(filter1.split(') '))
+    st.write(filter1, filter1.split(') '))
     
     fig.update_layout(legend=dict(itemwidth=30, title_text='', font_size=int(font_size_factor*25), ),
                       margin=dict(l=0, r=0, t=75, b=0),
