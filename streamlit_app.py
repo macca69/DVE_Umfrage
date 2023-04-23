21022c4801a2bc3b38e964f00d3193d12d98d53c
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-23 23:14:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def plot_and_layout(fig_data, filter1, filter2, barnorm, horizontal_flag, font_s
     if horizontal_flag:
         if color_scale_flag:
             fig = px.histogram(fig_data,
-                       y=filter1, x='counts', color=filter2, barnorm=barnorm, text_auto=True, color_discrete_sequence=px.colors.sequential.RdPu,
+                       y=filter1, x='counts', color=filter2, barnorm=barnorm, text_auto=True, color_discrete_sequence=px.colors.diverging.RdYlBu,
                        width=1000, height=750)
         else:
             fig = px.histogram(fig_data,
@@ -43,7 +43,7 @@ def plot_and_layout(fig_data, filter1, filter2, barnorm, horizontal_flag, font_s
     else:
         if color_scale_flag:
             fig = px.histogram(fig_data,
-                       x=filter1, y='counts', color=filter2, barnorm=barnorm, text_auto=True, color_discrete_sequence=px.colors.sequential.RdPu,
+                       x=filter1, y='counts', color=filter2, barnorm=barnorm, text_auto=True, color_discrete_sequence=px.colors.diverging.RdYlBu,
                        width=1000, height=750)
         else:
             fig = px.histogram(fig_data,
