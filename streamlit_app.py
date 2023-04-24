206d398626371ecdf80865b461627b89fbebc4a4
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-24 13:45:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,12 +65,13 @@ def significance_test(df, filter1, filter2, filter1_items, filter2_items):
         filter1_items = [x for x in filter1_items if x == x] # Drop nan
         filter2_items = [x for x in filter2_items if x == x] # Drop nan
         
-        st.write(np.array(pivot_table))
+        st.text('Beobachtete Häufigkeiten')
         st.dataframe(pd.DataFrame(np.array(pivot_table).astype(int), columns=pivot_table.columns, index=pivot_table.index),  use_container_width=True)
         
         st.text("Chi-Quadrat-Statistik = " + str(chi2_stat))
         st.text("p-Wert = " + str(p_val))
         st.text("Freiheitsgrade = " + str(dof))
+        
         st.text("Erwartete Häufigkeiten")
         st.dataframe(pd.DataFrame(np.array(expected).astype(int), columns=pivot_table.columns, index=pivot_table.index),  use_container_width=True)
 
