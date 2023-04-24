40698da78f2d7d2cc244d120a6923938adae89e8
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-24 12:24:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -178,7 +178,7 @@ if filter2 == 'keiner':
 else:
     
     with col3.expander('Kategorien wählen'):
-    filter1_items = st.multiselect('Kategorien wählen', natsorted(df_1[filter1].dropna().unique()), natsorted(df_1[filter1].dropna().unique()), label_visibility='collapsed')
+        filter1_items = st.multiselect('Kategorien wählen', natsorted(df_1[filter1].dropna().unique()), natsorted(df_1[filter1].dropna().unique()), label_visibility='collapsed')
 
     if filter2 in mehrfach: col4.markdown('Dieser Filter beinhaltet **:blue[Mehrfachnennungen]**')
 
