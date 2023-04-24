0e9611ed9a7b0c694d23d162785cc7f8e1201ff0
macca69 <47122251+macca69@users.noreply.github.com>
2023-04-24 13:01:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,11 +156,10 @@ if filter2 == 'keiner':
         with col3.expander('Kategorien wählen'):
             filter1_items = st.multiselect('Kategorien wählen', natsorted(df_1[filter1].dropna().unique()), natsorted(df_1[filter1].dropna().unique()), label_visibility='collapsed')
 
-        
-        font_size_factor = st.number_input('Schriftgröße', min_value=0.5, max_value=2.0, value=0.5, step=0.1, key='57')
-
         figure_data = df_1[df_1[filter1].isin(filter1_items)][filter1]
 
+    font_size_factor = st.number_input('Schriftgröße', min_value=0.5, max_value=2.0, value=0.5, step=0.1, key='57')
+    
     fig = px.histogram(figure_data, text_auto='.0f')
 
     fig.update_layout(showlegend=False,
